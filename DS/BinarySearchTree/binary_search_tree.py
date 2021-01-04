621ab2d17a9f9d5b4ef2fcd1acf260d9fba03d32
Dushyant Bhatt <dubhatt@microsoft.com>
2021-01-03 21:04:12 -0800

    BinarySearchTree delete element, find min, find max, pre order and post order traversal

--- DS/BinarySearchTree/binary_search_tree.py
+++ DS/BinarySearchTree/binary_search_tree.py
@@ -48,6 +48,97 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def post_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.post_order_traversal()
+
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    def pre_order_traversal(self):
+        elements = []
+
+        elements.append(self.data)
+
+        if self.left:
+            elements += self.left.pre_order_traversal()
+
+        if self.right:
+            elements += self.right.pre_order_traversal()
+
+        return elements
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_max()
+
+    def calculate_sum(self):
+        sum = 0
+
+        if self.left:
+            sum += self.left.calculate_sum()
+
+        sum += self.data
+
+        if self.right:
+            sum += self.right.calculate_sum()
+
+        return sum
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val=val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val=val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.right
+            if self.right is None:
+                return self.right
+
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = self.right.delete(min_val)
+
+        return self
+
+    def delete2(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete2(val=val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete2(val=val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            if self.left is None:
+                return self.left
+            if self.right is None:
+                return self.left
+
+            max_val = self.left.find_max()
+            self.data = max_val
+            self.left = self.left.delete2(max_val)
+
+        return self
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
@@ -64,3 +155,11 @@ if __name__ == "__main__":
     print(numbers_tree.in_order_traversal())
     print(numbers_tree.search(20))
     print(numbers_tree.search(22))
+    print(numbers_tree.find_min())
+    print(numbers_tree.find_max())
+    print(numbers_tree.calculate_sum())
+    print(numbers_tree.post_order_traversal())
+    print(numbers_tree.pre_order_traversal())
+    print(numbers_tree.delete(20))
+    print(numbers_tree.delete2(17))
+    print(numbers_tree.in_order_traversal())
